b10fc1112666d46e88cf76168b25e6e17136b1b9
Realchop <jovanovic.lazar002@gmail.com>
2023-08-07 23:03:45 +0200

    Setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'summit2023',
+    appId: '1:1041198559428:web:2a051b763ddbaf1f2f5991',
+    storageBucket: 'summit2023.appspot.com',
+    apiKey: 'AIzaSyAQGhXpOtEpDC10m_TEGYu6FVZMnPY6kF4',
+    authDomain: 'summit2023.firebaseapp.com',
+    messagingSenderId: '1041198559428',
+  },
   production: true
 };
