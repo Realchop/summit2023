50dce213e9f47415b44a17e7de3fe8a95068bf9d
Realchop <jovanovic.lazar002@gmail.com>
2023-08-08 18:04:47 +0200

    Implement a basic storage service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,15 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), IonicStorageModule.forRoot(),
+     AppRoutingModule, 
+     provideFirebaseApp(() => initializeApp(environment.firebase)), 
+     provideAuth(() => getAuth()), 
+     provideFirestore(() => getFirestore()), 
+     provideStorage(() => getStorage())
+    ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
