9bd6c77287521f4a31cf33d5dcbbd5069a17526c
Realchop <jovanovic.lazar002@gmail.com>
2023-08-10 17:53:34 +0200

    Prepare routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { LogoutGuard } from './core/logout.guard';
+import { Auth } from '@angular/fire/auth';
+import { StorageService } from './services/storage.service';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'main/profile',
-    pathMatch: 'full'
-  },
-  {
-    path: 'main/:id',
+    path: 'main',
+    // canLoad: [() => inject(Auth).currentUser ? true : false],
     loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
   },
   {
     path: 'logout',
-    canActivate: [LogoutGuard],
+    canMatch: [() => {
+      inject(Auth).signOut();
+      inject(StorageService).clear();
+      location.reload();
+    }],
     // Can be whatever, route won't ever be accessed
     loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
-  }
+  },
+  // All bad paths lead to profile
+  {
+    path: '**',
+    redirectTo: 'main/profile',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,10 +2,41 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainPage } from './main.page';
+import { EventsComponent } from './events/events.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'profile',
+    pathMatch: 'full'
+  },
+  // {
+  //   path: 'profile',
+  //   component: ProfileComponent,
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'agenda',
+  //   component: AgendaComponent,
+  //   pathMatch: 'full'
+  // },
+  {
+    path: 'events',
+    component: EventsComponent,
+    pathMatch: 'full'
+  },
+  // {
+  //   path: 'map',
+  //   component: MapComponent,
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'settings',
+  //   component: SettingsComponent,
+  //   pathMatch: 'full'
+  // },
+  {
+    path: ':id',
     component: MainPage
   }
 ];
