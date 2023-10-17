e0e90dd31643bb501610b5131378a126451c9197
Realchop <jovanovic.lazar002@gmail.com>
2023-10-17 14:47:49 +0200

    Edit agenda part 1

--- src/app/admin/manage-agenda/manage-agenda.component.ts
+++ src/app/admin/manage-agenda/manage-agenda.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AgendaService } from 'src/app/services/agenda.service';
 
 @Component({
   selector: 'app-manage-agenda',
   templateUrl: './manage-agenda.component.html',
   styleUrls: ['./manage-agenda.component.scss'],
 })
-export class ManageAgendaComponent  implements OnInit {
+export class ManageAgendaComponent {
+  public time: string = '';
+  public name: string = '';
+  public description: string = '';
+
+  private agendaService = inject(AgendaService);
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
